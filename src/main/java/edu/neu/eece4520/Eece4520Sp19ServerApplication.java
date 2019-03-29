78340abdea603dfc61ea9165c78c97f916ee2247
Bailey Kane <bailey.orion.kane@gmail.com>
2019-03-29 10:42:26 -0400

    First commit

---
+++ src/main/java/edu/neu/eece4520/Eece4520Sp19ServerApplication.java
@@ -0,0 +1,13 @@
+package edu.neu.eece4520;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Eece4520Sp19ServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Eece4520Sp19ServerApplication.class, args);
+    }
+
+}
